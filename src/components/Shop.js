54aa384c77a7cd846b2e508433bafb707aea083d
Disha Sahu <sahu.disha21@gmail.com>
2025-10-04 00:52:20 +0530

    Implemted balance in Shop as well by using useSelector same as Navbar

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { actionCreators } from '../state/index'
 import { bindActionCreators } from 'redux'
 
 const Shop = () => {
     const dispatch = useDispatch();
     const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch);
+
+    const balance = useSelector(state => state.amount)
 
     return (
         <div className='container my-3'>
@@ -16,7 +18,7 @@ const Shop = () => {
 
             {/* Implemented using bindCreators and destructuring i.e. by extracting the value */}
             <button className="btn btn-primary mx-2" onClick={() => { withdrawMoney(100) }}>-</button>
-            Updated Balance
+            Updated Balance ({balance})
             <button className="btn btn-primary mx-2" onClick={() => { depositMoney(100) }}>+</button>
         </div>
     )
