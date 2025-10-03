2cb8eec6356a41be5db420b2b803b7abb8f69cda
Disha Sahu <sahu.disha21@gmail.com>
2025-10-04 00:50:37 +0530

    Implemented bind Creators to short function using

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { actionCreators } from '../state/index'
+import { bindActionCreators } from 'redux'
 
 const Shop = () => {
     const dispatch = useDispatch();
+    const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch);
+
     return (
         <div className='container my-3'>
             <h2>Update Bank Balance for Deposit/Withdrawl</h2>
-            <button className="btn btn-primary mx-2" onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }}>-</button>
+            {/* <button className="btn btn-primary mx-2" onClick={() => { dispatch(actionCreators.withdrawMoney(100)) }}>-</button>
             Updated Balance
-            <button className="btn btn-primary mx-2" onClick={() => { dispatch(actionCreators.depositMoney(100)) }}>+</button>
+            <button className="btn btn-primary mx-2" onClick={() => { dispatch(actionCreators.depositMoney(100)) }}>+</button> */}
+
+            {/* Implemented using bindCreators and destructuring i.e. by extracting the value */}
+            <button className="btn btn-primary mx-2" onClick={() => { withdrawMoney(100) }}>-</button>
+            Updated Balance
+            <button className="btn btn-primary mx-2" onClick={() => { depositMoney(100) }}>+</button>
         </div>
     )
 }
